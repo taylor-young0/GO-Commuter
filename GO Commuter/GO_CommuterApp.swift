aec412b5693bd8f5183c8cd2d9f27036a5c64957
Taylor Young <59883690+taylor-young0@users.noreply.github.com>
2023-09-01 12:59:50 -0400

    Initial Commit

---
+++ GO Commuter/GO_CommuterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GO_CommuterApp.swift
+//  GO Commuter
+//
+//  Created by Taylor Young on 2023-09-01.
+//
+
+import SwiftUI
+
+@main
+struct GO_CommuterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
